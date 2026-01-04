e42e92638a54daba7678fb77282d61620797c697
Dattatray Gatkal <133833927+dattatray-gatkal@users.noreply.github.com>
2026-01-04 16:03:15 +0530

    Feature/dashboard design (#1)
    * started dashboard designing
    
    * started dashboard designing

--- gitpracticenextapplication/src/app/dashboard/page.tsx
+++ gitpracticenextapplication/src/app/dashboard/page.tsx
@@ -4,8 +4,10 @@ const page = () => {
   return (
     <div>
       <h1>user dashboard</h1>
+      <h1>developer one started designing the dashboard</h1>
     </div>
   );
 };
 
 export default page;
+
