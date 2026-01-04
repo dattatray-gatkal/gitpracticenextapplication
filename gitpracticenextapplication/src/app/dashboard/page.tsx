0d389e9b7b17fa7c919132e1cd0aebcaad153177
dattatray-gatkal <dattatraygatkal74@gmail.com>
2026-01-04 17:02:05 +0530

    added code inside dashboard

--- gitpracticenextapplication/src/app/dashboard/page.tsx
+++ gitpracticenextapplication/src/app/dashboard/page.tsx
@@ -3,11 +3,13 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1>user dashboard</h1>
-      <h1>developer one started designing the dashboard</h1>
+      <h1>User Dashboard</h1>
+      <h2>Developer one started designing the dashboard</h2>
+      <h2>Dashboard layout in progress</h2>
+      <h2>Adding widgets and charts</h2>
+      <h2>Working on responsiveness</h2>{" "}
     </div>
   );
 };
 
 export default page;
-
