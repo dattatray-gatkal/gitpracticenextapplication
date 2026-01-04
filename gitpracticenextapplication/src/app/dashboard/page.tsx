c0dcd92246ed8aa79db734baf7727c8e203e345d
dattatray-gatkal <dattatraygatkal74@gmail.com>
2026-01-04 17:06:37 +0530

    dev2 changed the code of dashboard

--- gitpracticenextapplication/src/app/dashboard/page.tsx
+++ gitpracticenextapplication/src/app/dashboard/page.tsx
@@ -5,9 +5,10 @@ const page = () => {
     <div>
       <h1>user dashboard</h1>
       <h1>developer one started designing the dashboard</h1>
+      <h2>developer 2 has its own dashboard code</h2>
+      <h2>updated profile code stuff</h2>
     </div>
   );
 };
 
 export default page;
-
